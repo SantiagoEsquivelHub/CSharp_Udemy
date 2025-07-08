dc88a56aab7a58e1393dc201b759a8a4e5b6c49e
Santiago Sánchez Esquivel <ssancheze@siesa.com>
2025-07-08 09:42:26 -0500

    first commit

---
+++ HelloApp/01-bases/Variables.cs
@@ -0,0 +1,11 @@
+partial class Program
+{
+    static void Variables()
+    {
+        int age = 30;
+        string name = "Santiago";
+        bool isStudent = true;
+
+        Console.WriteLine($"Name: {name}, is {age} years old and he is student {isStudent}");
+    }
+}
