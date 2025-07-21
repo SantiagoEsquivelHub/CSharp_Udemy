5555f399262daad52971a1e326e3ed6b98516f60
Santiago Sánchez Esquivel <ssancheze@siesa.com>
2025-07-21 15:02:50 -0500

    SS - Finish TaskMasterAPI

---
+++ TaskMasterAPI/Controllers/TaskController.cs
@@ -0,0 +1,76 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+using TaskMasterAPI.Models;
+using TaskMasterAPI.Services;
+
+namespace TaskMasterAPI.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TaskController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<Models.Task>> GetTasks()
+    {
+        return Ok(TaskDataStore.Current.Tasks);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Models.Task> GetTask(int id)
+    {
+        var task = TaskDataStore.Current.Tasks.FirstOrDefault(t => t.Id == id);
+
+        if (task == null)
+            return NotFound("Task has not found");
+
+        return Ok(task);
+    }
+
+    [HttpPost]
+    public ActionResult<Models.Task> CreateTask(Models.TaskInsert taskInsert)
+    {
+        var newTask = new Models.Task
+        {
+            Id = TaskDataStore.Current.Tasks.Max(t => t.Id + 1),
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now,
+            IsCompleted = false,
+            Title = taskInsert.Title,
+            Description = taskInsert.Description,
+        };
+
+        TaskDataStore.Current.Tasks.Add(newTask);
+        return Ok(newTask);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<Models.Task> UpdateTask(int id, Models.TaskInsert taskInsert)
+    {
+        var task = TaskDataStore.Current.Tasks.FirstOrDefault(t => t.Id == id);
+
+        if (task == null)
+            return NotFound("Task has not found");
+
+        task.Title = taskInsert.Title;
+        task.Description = taskInsert.Description;
+        task.CreatedAt = DateTime.Now;
+        task.CreatedAt = DateTime.Now;
+
+        return Ok(task);
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult DeleteTask(int id)
+    {
+        var task = TaskDataStore.Current.Tasks.FirstOrDefault(t => t.Id == id);
+
+        if (task == null)
+            return NotFound("Task has not found");
+
+        TaskDataStore.Current.Tasks.Remove(task);
+        return NoContent();
+    }
+
+
+}
